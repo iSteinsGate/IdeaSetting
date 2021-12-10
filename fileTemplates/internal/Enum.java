88c6e9edf482a8a14adbf8fbab85ff6937533d82
tanqinghui <tanqinghui1995@163.com>
2021-12-10 10:08:24 +0800

    IU-2020.3 <tanqinghui@ZERO Create debugger.xml, updates.xml, Enum.java, Interface.java, keymap.xml, editor.xml, alibaba-toolkit-startUp.xml, projectView.xml, Default.xml, ide.general.xml, project.default.xml, user_eyx.xml, wsl.distributions.xml, pluginAdvertiser.xml, laf.xml, web-browsers.xml, usageView.xml, ide-features-trainer.xml, Default.xml, codota.xml, yiiguxing.translation.xml, jdk.table.xml, lightEdit.xml, vim_settings.xml, filetypes.xml, terminal.xml, equalsHashCodeTemplates.xml, templates.xml, CodeGlance.xml, colors.scheme.xml, databaseDrivers.xml, Default.xml, user.xml, alibaba-toolkit-region.xml, baseRefactoring.xml, qaPlugPluginUpdateNotifier.xml, KeyPromoterXSettings.xml, markdown.xml, path.macros.xml, toStringTemplates.xml, ui.lnf.xml, Windows copy.xml, customization.xml, VisualVMLauncher.xml, user_his.xml, Class.java, vcs.xml, mybatis.xml, javarebel.xml, find.xml

---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+* 
+* @author tanqinghui
+* @since ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+**/
+public enum ${NAME} {
+}
